988cf938917e73db1c4a680283fe399e8e7ed81d
Slava Bacherikov <slava@bacher09.org>
2012-05-28 13:22:48 +0300

    Add more models to admin.

--- gpackages/apps/packages/admin.py
+++ gpackages/apps/packages/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
-from models import EbuildModel
+from models import EbuildModel, PackageModel, LicensModel, CategoryModel, \
+                   UseFlagModel, MetaDataModel, RepositoryModel
 
 admin.site.register(EbuildModel)
+admin.site.register(PackageModel)
+admin.site.register(LicensModel)
+admin.site.register(CategoryModel)
+admin.site.register(UseFlagModel)
+admin.site.register(RepositoryModel)
+admin.site.register(MetaDataModel)
