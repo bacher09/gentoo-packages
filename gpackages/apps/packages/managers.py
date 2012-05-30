cd6f1e10b426ab50ae4d0d9040adf3e07f6747b1
Slava Bacherikov <slava@bacher09.org>
2012-05-30 15:08:29 +0300

    Optimize getting by object.

--- gpackages/apps/packages/managers.py
+++ gpackages/apps/packages/managers.py
@@ -13,17 +13,15 @@ def _gen_query_and_manager(MixinClass, QueryClassName, ManagerClassName):
 class PackageMixin(object):
     def get(self, package = None, *args, **kwargs):
         if package is not None and isinstance(package, Package):
-            try:
-                category = packages.models.CategoryModel.objects.get(category = package.category)
-            except packages.models.CategoryModel.DoesNotExist:
-                raise self.model.DoesNotExist
+            if 'category' not in kwargs:
+                kwargs.update({'category__category' : package.category})
             name = package.name
             if len(args)>=1:
                 args[0] = name
             if len(args)>=2:
                 args[1] = category
             else:
-                kwargs.update({'name': name, 'category': category})
+                kwargs.update({'name': name})
         return super(PackageMixin, self).get(*args, **kwargs)
 
 PackageQuerySet, PackageManager = _gen_query_and_manager(PackageMixin, 
@@ -68,10 +66,10 @@ class EbuildMixin(object):
             try:
                 params = dict([(k, v) for k, v in kwargs.items() if '__' not in k])
                 params.update(defaults)
+                sid = transaction.savepoint(using=self.db)
                 obj = self.model(**params)
                 if 'ebuild' not in kwargs:
                     obj.save(force_insert=True, using=self.db)
-                sid = transaction.savepoint(using=self.db)
                 transaction.savepoint_commit(sid, using=self.db)
                 return obj, True
             except IntegrityError as e:
@@ -84,17 +82,18 @@ class EbuildMixin(object):
                     raise exc_info[1], None, exc_info[2]
     
     
-    def get(self, ebuild=None, *args, **kwargs):
+    def get(self, ebuild=None,package = None, *args, **kwargs):
         if ebuild is not None and isinstance(ebuild, Ebuild):
-            try:
-                package = packages.models.PackageModel.objects.get(package = ebuild.package)
-            except packages.models.PackageModel.DoesNotExist:
-                raise self.model.DoesNotExist
+            if package is None:
+                kwargs.update({
+                        'package__category__category': ebuild.package.category,
+                        'package__name': ebuild.package.name })
+            else:
+                kwargs.update({'package': package})
             version = ebuild.version
             revision = ebuild.revision
             kwargs.update({ 'version': version,
-                            'revision': revision,
-                            'package': package })
+                            'revision': revision })
         return super(EbuildMixin, self).get(*args, **kwargs)
             
 
