0ea02f6f29cbcea12e249f882f6f7b723920738b
Slava Bacherikov <slava@bacher09.org>
2012-06-07 18:27:26 +0300

    Code clean-up in admin

--- gpackages/apps/packages/admin.py
+++ gpackages/apps/packages/admin.py
@@ -4,6 +4,25 @@ from models import EbuildModel, PackageModel, LicensModel, CategoryModel, \
                    UseFlagModel,  RepositoryModel, HomepageModel, HerdsModel, \
                    MaintainerModel, Keyword, ArchesModel, UseFlagDescriptionModel
 
+class AbstractAnnotateAdmin(object):
+    annotate_dict = {}
+
+    def queryset(self, request):
+        return super(AbstractAnnotateAdmin, self).queryset(request) \
+            .annotate(**self.annotate_dict)
+
+class EbuildsCountAdmin(AbstractAnnotateAdmin):
+    annotate_dict = {'ebuilds_count': Count('ebuildmodel')}
+    
+    def ebuilds_count(self, obj):
+        return obj.ebuilds_count
+
+class PackagesCountAdmin(AbstractAnnotateAdmin):
+    annotate_dict = {'packages_count': Count('packagemodel')}
+
+    def packages_count(self, obj):
+        return obj.packages_count
+
 class EbuildAdmin(admin.ModelAdmin):
     list_display = ('__unicode__', 'is_masked', )
     list_filter = ('created_datetime','updated_datetime', 'licenses', 'is_masked')
@@ -11,49 +30,22 @@ class EbuildAdmin(admin.ModelAdmin):
     date_hierarchy = 'updated_datetime'
     list_select_related = True
 
-
-class PackageAdmin(admin.ModelAdmin):
+class PackageAdmin(EbuildsCountAdmin, admin.ModelAdmin):
     list_display = ('__unicode__', 'ebuilds_count')
     list_filter = ('created_datetime', 'updated_datetime', 'herds')
     list_select_related = True
 
-    def queryset(self, request):
-        return super(PackageAdmin, self).queryset(request).annotate(ebuilds_count = Count('ebuildmodel'))
-
-    def ebuilds_count(self, obj):
-        return obj.ebuilds_count
-
-
-class HerdsAdmin(admin.ModelAdmin):
+class HerdsAdmin(PackagesCountAdmin, admin.ModelAdmin):
     list_display = ('name', 'email', 'description', 'packages_count')
     search_fields = ('name', 'email')
 
-    def queryset(self, request):
-        return super(HerdsAdmin, self).queryset(request).annotate(packages_count = Count('packagemodel'))
-    
-    def packages_count(self, obj):
-        return obj.packages_count
-
-class MaintainerAdmin(admin.ModelAdmin):
+class MaintainerAdmin(PackagesCountAdmin, admin.ModelAdmin):
     list_display = ('name', 'email', 'packages_count')
     search_fields = ('name', 'email')
 
-    def queryset(self, request):
-        return super(MaintainerAdmin, self).queryset(request).annotate(packages_count = Count('packagemodel'))
-
-    def packages_count(self, obj):
-        return obj.packages_count
-
-
-class UseFlagAdmin(admin.ModelAdmin):
+class UseFlagAdmin(EbuildsCountAdmin, admin.ModelAdmin):
     list_display = ('name', 'description', 'ebuilds_count')
     search_fields = ('name', 'description')
-
-    def queryset(self, request):
-        return super(UseFlagAdmin, self).queryset(request).annotate(ebuilds_count = Count('ebuildmodel'))
-
-    def ebuilds_count(self, obj):
-        return obj.ebuilds_count
 
 class UseFlagDescriptionAdmin(admin.ModelAdmin):
     list_display = ('use_flag', 'package', 'description')
@@ -63,15 +55,10 @@ class HomepageAdmin(admin.ModelAdmin):
     list_display = ('url',)
     search_fields = ('url',)
 
-class LicensAdmin(admin.ModelAdmin):
-    list_display = ('name', 'licenses_count')
+class LicensAdmin(EbuildsCountAdmin, admin.ModelAdmin):
+    list_display = ('name', 'ebuilds_count')
     search_fields = ('name',)
 
-    def queryset(self, request):
-        return super(LicensAdmin, self).queryset(request).annotate(licenses_count = Count('ebuildmodel'))
-    
-    def licenses_count(self, obj):
-        return obj.licenses_count
 
 admin.site.register(EbuildModel, EbuildAdmin)
 admin.site.register(PackageModel, PackageAdmin)
