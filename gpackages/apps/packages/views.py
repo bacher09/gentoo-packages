70c4b033c57841206842260288068d240cbf59e2
Slava Bacherikov <slava@bacher09.org>
2012-06-16 02:27:40 +0300

    Add activity indicator

--- gpackages/apps/packages/views.py
+++ gpackages/apps/packages/views.py
@@ -13,26 +13,26 @@ class ContextListView(ListView):
         return ret
 
 class CategoriesListView(ContextListView):
-    extra_context = {'page_name': 'Categories'}
+    extra_context = {'page_name': 'Categories', 'nav_name' : 'categories'}
     template_name = 'categories.html'
     queryset = CategoryModel.objects.defer('metadata_hash').all()
     context_object_name = 'categories'
 
 class HerdsListView(ContextListView):
-    extra_context = {'page_name': 'Herds'}
+    extra_context = {'page_name': 'Herds', 'nav_name': 'herds'}
     template_name = 'herds.html'
     queryset = HerdsModel.objects.only('name', 'email', 'description').all()
     context_object_name = 'herds'
 
 class MaintainersListView(ContextListView):
     paginate_by = 40
-    extra_context = {'page_name': 'Maintainers'}
+    extra_context = {'page_name': 'Maintainers', 'nav_name': 'maintainers'}
     template_name = 'maintainers.html'
     queryset = MaintainerModel.objects.only('name', 'email' ).all()
     context_object_name = 'maintainers'
 
 class RepositoriesListView(ContextListView):
-    extra_context = {'page_name': 'Repsitories'}
+    extra_context = {'page_name': 'Repsitories', 'nav_name': 'repositories'}
     template_name = 'repositories.html'
     queryset = RepositoryModel.objects.only('name', 'description' ).all()
     context_object_name = 'repositories'
