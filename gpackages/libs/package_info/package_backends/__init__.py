cdb05942e13b61385180882e0a966a58489204f0
Slava Bacherikov <slava@bacher09.org>
2012-06-22 03:15:21 +0300

    Change in backend loading

--- gpackages/libs/package_info/package_backends/__init__.py
+++ gpackages/libs/package_info/package_backends/__init__.py
@@ -7,7 +7,7 @@ def import_backend(backend):
     backend_module = __import__(__name__, fromlist=[backend], level = 0) 
     return backend_module
 
-def get_backend():
+def get_backend_name():
     backend = DEFAULT_BACKEND
     try:
         from main import settings
@@ -19,7 +19,9 @@ def get_backend():
             backend = settings.PACKAGE_INFO_BACKEND
         except AttributeError:
             pass
+    return backend
 
+def load_backend(backend):
     try:
         backend_module = import_backend(backend)
     except ImportError:
@@ -32,3 +34,7 @@ def get_backend():
 
     portage = backend_module.Portage()
     return portage
+
+def get_backend():
+    backend = get_backend_name()
+    return load_backend(backend)
