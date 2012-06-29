ec64f86aadb7bb5895ef851a73c2f6c69f7603e0
Slava Bacherikov <slava@bacher09.org>
2012-06-29 23:49:51 +0300

    Change in general filter.

--- gpackages/apps/generic/views.py
+++ gpackages/apps/generic/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import TemplateView, ListView
+from string import Template
 
 class ContextView(object):
     extra_context = {}
@@ -18,3 +19,73 @@ class SetLang(ContextTemplateView):
     extra_context = {'page_name': 'Set Language', 'nav_name' : 'setlanguage'}
 
 set_lang_view = SetLang.as_view()
+
+# there is another dynamic filter for django, and it maybe better 
+# but it is too big and i need just a litle of its functionly
+# but if this code have to be grove maybe i replace it to django-filter
+# application or another.
+def dynamic_filter(filter_set, allowed):
+    result = {}
+    for k in allowed.iterkeys():
+        if k in filter_set:
+            result[allowed[k]] = filter_set[k]
+    return result
+
+def exclude_blank(res_dict):
+    result = {}
+    for k in res_dict.iterkeys():
+        if res_dict[k]:
+            result[k] = res_dict[k]
+    return result
+
+def dynamic_order(args_list, allowed_list, reverse = None):
+    order = allowed_list.get(None)
+    if reverse is None:
+        reverse = args_list.get('reverse', False)
+    if args_list.get('order') in allowed_list:
+        order = allowed_list.get(args_list.get('order'))
+
+    if order == '?':
+        return order
+    
+    if reverse and order[0] != '-':
+        order = '-' + order
+    elif reverse:
+        order =  order[1:]
+    return order
+
+class MultipleFilterListViewMixin(object):
+    allowed_filter = {}
+    allowed_order = {}
+
+    base_queryset = None # should be queryset
+
+    def get_queryset(self):
+        qs = dynamic_filter(exclude_blank(self.request.GET),
+                                        self.allowed_filter)
+        qs.update( dynamic_filter(exclude_blank(self.kwargs),
+                                        self.allowed_filter) )
+        
+        if self.kwargs.get('rev') is None:
+            reverse = bool(self.request.GET.get('rev',False))
+        else:
+            reverse = bool(self.kwargs.get('rev',False))
+        
+        if 'order' in self.request.GET:
+            order = dynamic_order(self.request.GET, self.allowed_order,reverse)
+        else:
+            order = dynamic_order(self.kwargs, self.allowed_order, reverse)
+            if self.kwargs.get('order') not in self.allowed_order:
+                raise Http404('no such order')
+
+        return self.base_queryset.filter(**qs).order_by(order)
+
+    @classmethod
+    def get_url_part(cls):
+        t = "(?:{0}/(?P<{0}>[^/]+)/)?"
+        l =[]
+        for key in cls.allowed_filter.iterkeys():
+            l.append(t.format(key))
+
+        return ''.join(l) + "(?:order/(?P<order>[a-z]*)/)?(?P<rev>rev/)?"
+            
--- gpackages/apps/packages/views.py
+++ gpackages/apps/packages/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import DetailView
-from generic.views import ContextListView, ContextTemplateView, ContextView
+from generic.views import ContextListView, ContextTemplateView, ContextView, \
+                          MultipleFilterListViewMixin
 from models import CategoryModel, HerdsModel, MaintainerModel, \
                    RepositoryModel, LicenseGroupModel, EbuildModel, \
                    PackageModel
@@ -48,45 +49,11 @@ class EbuildsListView(ContextListView):
                        'package__virtual_package',
                        'package__virtual_package__category').prefetch_keywords(arches)
 
-# there is another dynamic filter for django, and it maybe better 
-# but it is too big and i need just a litle of its functionly
-# but if this code have to be grove maybe i replace it to django-filter
-# application or another.
-def dynamic_filter(filter_set, allowed):
-    result = {}
-    for k in allowed.iterkeys():
-        if k in filter_set:
-            result[allowed[k]] = filter_set[k]
-    return result
-
-def exclude_blank(res_dict):
-    result = {}
-    for k in res_dict.iterkeys():
-        if res_dict[k]:
-            result[k] = res_dict[k]
-    return result
-
-def dynamic_order(args_list, allowed_list, reverse = None):
-    order = allowed_list.get(None)
-    if reverse is None:
-        reverse = args_list.get('reverse', False)
-    if args_list.get('order') in allowed_list:
-        order = allowed_list.get(args_list.get('order'))
-
-    if order == '?':
-        return order
-    
-    if reverse and order[0] != '-':
-        order = '-' + order
-    elif reverse:
-        order =  order[1:]
-    return order
-
-class PackagesListsView(ContextListView):
+class PackagesListsView(MultipleFilterListViewMixin, ContextListView):
     allowed_filter = { 'category':'virtual_package__category__category',
                        'repo':'repository__name',
                        'herd':'herds__name',
-                       'maintainer_pk': 'maintainers__pk',
+                       'maintainer': 'maintainers__pk',
                        'license': 'licenses__name'
                     }
 
@@ -113,26 +80,6 @@ class PackagesListsView(ContextListView):
                        'virtual_package__category'). \
         prefetch_related('repository'). \
         prefetch_keywords(arches)
-
-    def get_queryset(self):
-        qs = dynamic_filter(exclude_blank(self.request.GET),
-                                        self.allowed_filter)
-        qs.update( dynamic_filter(exclude_blank(self.kwargs),
-                                        self.allowed_filter) )
-        
-        if self.kwargs.get('rev') is None:
-            reverse = bool(self.request.GET.get('rev',False))
-        else:
-            reverse = bool(self.kwargs.get('rev',False))
-        
-        if 'order' in self.request.GET:
-            order = dynamic_order(self.request.GET, self.allowed_order,reverse)
-        else:
-            order = dynamic_order(self.kwargs, self.allowed_order, reverse)
-            if self.kwargs.get('order') not in self.allowed_order:
-                raise Http404('no such order')
-
-        return self.base_queryset.filter(**qs).order_by(order)
 
 class PackageDetailView(ContextView, DetailView):
     arches = ['alpha', 'amd64', 'arm', 'hppa', 'ia64', 'ppc', 'ppc64', 'sparc', 'x86']
