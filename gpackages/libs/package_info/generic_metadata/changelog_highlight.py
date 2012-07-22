8fab4fd9cc595500be935386ccc6cc7fc6216760
Slava Bacherikov <slava@bacher09.org>
2012-07-22 23:42:12 +0300

    Add style for changelog highlight

--- gpackages/libs/package_info/generic_metadata/changelog_highlight.py
+++ gpackages/libs/package_info/generic_metadata/changelog_highlight.py
@@ -1,4 +1,7 @@
 from pygments.lexer import RegexLexer, bygroups
+from pygments.formatters import HtmlFormatter
+from pygments.formatters.html import _escape_html_table
+from pygments.style import Style
 from pygments.token import *
 import re
 
@@ -67,4 +70,79 @@ class ChangelogLexer(RegexLexer):
             (r'[^#\n ]+', Generic),
             (r' +', Whitespace),
         ]
+    }
+
+class ChangelogHtmlFormater(HtmlFormatter):
+    def _format_lines(self, tokensource):
+        """
+        Copyed from pygments source litle modified.
+        """
+        nocls = self.noclasses
+        lsep = self.lineseparator
+        # for <span style=""> lookup only
+        getcls = self.ttype2class.get
+        c2s = self.class2style
+        escape_table = _escape_html_table
+
+        lspan = ''
+        line = ''
+        for ttype, value in tokensource:
+            if nocls:
+                cclass = getcls(ttype)
+                while cclass is None:
+                    ttype = ttype.parent
+                    cclass = getcls(ttype)
+                cspan = cclass and '<span style="%s">' % c2s[cclass][0] or ''
+            else:
+                cls = self._get_css_class(ttype)
+                cspan = cls and '<span class="%s">' % cls or ''
+
+            parts = value.translate(escape_table).split('\n')
+
+            # for all but the last line
+            for part in parts[:-1]:
+                part = self.token_decorate(ttype, part)
+                if line:
+                    if lspan != cspan:
+                        line += (lspan and '</span>') + cspan + part + \
+                                (cspan and '</span>') + lsep
+                    else: # both are the same
+                        line += part + (lspan and '</span>') + lsep
+                    yield 1, line
+                    line = ''
+                elif part:
+                    yield 1, cspan + part + (cspan and '</span>') + lsep
+                else:
+                    yield 1, lsep
+            # for the last line
+            last = self.token_decorate(ttype, parts[-1])
+            if line and last:
+                if lspan != cspan:
+                    line += (lspan and '</span>') + cspan + last
+                    lspan = cspan
+                else:
+                    line += last 
+            elif last:
+                line = cspan + last
+                lspan = cspan
+            # else we neither have to open a new span nor set lspan
+
+        if line:
+            yield 1, line + (lspan and '</span>') + lsep
+
+    def token_decorate(self, token, value):
+        return value
+
+class ChangelogStyle(Style):
+    default_style = ""
+    styles = {
+        Comment:                'italic #888',
+        Date:                   '#F90',
+        FilePlus:               'bold #0F0',
+        FileMinus:              'bold #F00',
+        File:                   'bold #C0A',
+        PackageName:            'bold',
+        Bug:                    '#E00',
+        Email:                  '#0CF',
+        AuthorName:             'bold #1C9',
     }
