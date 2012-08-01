a4e05092c97f7d36a9e98110230e9f8f30a6d53c
Slava Bacherikov <slava@bacher09.org>
2012-08-02 02:22:46 +0300

    Add function for access to special use description

--- gpackages/libs/package_info/generic_metadata/use_info.py
+++ gpackages/libs/package_info/generic_metadata/use_info.py
@@ -1,14 +1,18 @@
 import re
 from collections import defaultdict
+import os.path
+import os
 
-__all__ = ('get_uses_info', 'get_local_uses_info')
+__all__ = ('get_uses_info', 'get_local_uses_info', 'get_use_special_info')
 
 USES_RE = r'(?P<use>[a-zA-Z0-9\-]+) - (?P<description>.*)'
 USES_DESC_RE = r'^%s$' % USES_RE
 USES_LOCAL_DESC_RE = r'^(?P<package>[^#].*):%s$' % USES_RE
+DESC_USE_RE = r'^(?P<name>[^\.]+)\.desc$'
 
 use_re = re.compile(USES_DESC_RE)
 use_local_re = re.compile(USES_LOCAL_DESC_RE)
+desc_re = re.compile(DESC_USE_RE)
 
 def _get_info(filename, re_string, modify_function, res_var = {}):
     use_desc = open(filename, 'r').read()
@@ -51,3 +55,23 @@ def get_local_uses_info(filename = '/usr/portage/profiles/use.local.desc'):
         res_dict[match['use'].lower()][match['package']] = match['description']
 
     return _get_info(filename, use_local_re, action, defaultdict(dict))
+
+def _set_prefixes(prefix, dct):
+    newdct = {}
+    for key, item in dct.iteritems():
+        newdct['%s_%s' % (prefix, key)] = item
+    return newdct
+
+def get_use_special_info(dirname = '/usr/portage/profiles/desc'):
+    uses = {}
+    for name in os.listdir(dirname):
+        m = desc_re.match(name)
+        if m is not None:
+            prefix = m.groupdict().get('name')
+            if prefix is None:
+                continue
+            filename = os.path.join(dirname, name)
+            uses.update(_set_prefixes(prefix, get_uses_info(filename)))
+    return uses
+
+
